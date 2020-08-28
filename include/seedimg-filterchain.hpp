b5abdfc4136305a3d9b9c484f399f2599ae3eeb0
Santanu <bsantanu381@gmail.com>
2020-08-28 22:37:12 +0530

    Experimental lazily-evaluated filterchain algorithm.

---
+++ include/seedimg-filterchain.hpp
@@ -0,0 +1,32 @@
+﻿#ifndef _SEEDIMG_FILTERCHAIN
+#define _SEEDIMG_FILTERCHAIN
+
+#include <seedimg.hpp>
+
+/**
+ * Filter is an abstract filter when called with an image
+ * does somekind of processing then modifies the image.
+ */
+class filter {
+public:
+  virtual void operator()(simg& input, simg& output) = 0;
+
+  simg operator()(simg& input) {
+    auto output = seedimg::make(input->width(),
+                                input->height());
+
+    (*this)(input, output);
+    return output;
+  }
+};
+
+class lazy_filterchain {
+private:
+  std::vector<std::reference_wrapper<filter>> filters;
+public:
+  lazy_filterchain() = default;
+  lazy_filterchain operator<<(filter& f);
+  void operator()(simg& input, simg& output);
+};
+
+#endif
