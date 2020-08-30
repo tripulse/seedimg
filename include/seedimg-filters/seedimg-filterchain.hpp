ca21c3189d9ae4d4743bf42773feaec57bcc0e67
Santanu <bsantanu381@gmail.com>
2020-08-30 22:04:36 +0530

    Switch to function-based filters; this adheres to the current idiom.
    Inplace filters aren't supported yet, requires metaprogramming for that.
    
    Regular methods for lazy_filterchain class over operator-overloading.

--- include/seedimg-filters/seedimg-filterchain.hpp
+++ include/seedimg-filters/seedimg-filterchain.hpp
@@ -6,27 +6,27 @@
 
 
 namespace seedimg::filters {
-class lazy_filterchain;
+//class lazy_filterchain;
 
-class filter {
-public:
-  virtual void operator()(const simg& input, simg& output) = 0;
+//class filter {
+//public:
+//  virtual void operator()(const simg& input, simg& output) = 0;
 
-  std::reference_wrapper<filter> operator>>(simg& input) {
-    (*this)(input, input);
-    return *this;
-  }
+//  std::reference_wrapper<filter> operator>>(simg& input) {
+//    (*this)(input, input);
+//    return *this;
+//  }
 
-  lazy_filterchain operator>>(filter&& f);
+//  lazy_filterchain operator>>(filter&& f);
 
-  //// this can enable eager evaluation of filters:
-  //  simg operator+(const simg& input) {
-  //    auto output = seedimg::make(input->width(),
-  //                                input->height());
-  //    (*this)(input, output);
-  //    return output;
-  //  }
-};
+//  //// this can enable eager evaluation of filters:
+//  //  simg operator+(const simg& input) {
+//  //    auto output = seedimg::make(input->width(),
+//  //                                input->height());
+//  //    (*this)(input, output);
+//  //    return output;
+//  //  }
+//};
 
 /** 
  * A linear chain of multiple filters which is lazily evaluated. For it being
@@ -35,15 +35,60 @@ public:
  */
 class lazy_filterchain {
 protected:
-  std::list<std::reference_wrapper<filter>> filters;
+//  std::list<std::reference_wrapper<filter>> filters;
+
+  std::list<std::function<void(simg&, simg&)>> filters;
   bool as_backwards;
 public:
-  lazy_filterchain() = default;
-  lazy_filterchain operator>>(filter&& f);
-  lazy_filterchain operator>>(simg& image);
-  lazy_filterchain operator()(const simg& input, simg& second);
-  lazy_filterchain operator--();
-  lazy_filterchain operator!();
+  /**
+   * @brief Push a function to end of the queue that follows
+   * the idioms of seedimg filter-function definitions. It
+   * will bind any number of additional arguments to the
+   * function (if given).
+   *
+   * @param func a callable object.
+   * @param args custom arguments to pass to callable.
+   */
+
+  // Why inlining?
+  // https://stackoverflow.com/q/10978642
+
+  // NOTE: excluding rvalue in 'args' causes the elements to be
+  // uninitialised for a very strange reason.
+  template<class F, class... Args>
+  lazy_filterchain& push_end(F&& func, Args&&... args) {
+    filters.push_back(
+          [&](simg& input, simg& output) -> void
+          { func(input, output, args...); });
+    return *this;
+  }
+
+  /**
+   * @brief Evaluate the filters in the queue in a defined order,
+   * by default it does in forward order backwards can be set by
+   * .reverse() method. If the queue was empty the resulting image
+   * is the same as the given one.
+   *
+   * @param input the initial image to apply the filters.
+   * @param output dump the resulting image.
+   */
+  lazy_filterchain& evaluate(const simg& input, simg& output);
+
+  /**
+   * @brief Same as the other overload, except its done inplace.
+   */
+  lazy_filterchain& evaluate(simg& image);
+
+  /**
+   * @brief Pop-off a filter from the very end of the queue.
+   */
+  lazy_filterchain& pop_end();
+
+  /**
+   * @brief Reverses the evaulation order, but doesn't reverse the
+   * actual ordering of filters in the internal queue. Sets a flag.
+   */
+  lazy_filterchain& reverse();
 };
 }
 
--- src/seedimg-filters/seedimg-filterchain/seedimg-filterchain.cpp
+++ src/seedimg-filters/seedimg-filterchain/seedimg-filterchain.cpp
@@ -2,41 +2,41 @@
 #include <seedimg-filters/seedimg-filterchain-ports.hpp>
 
 namespace seedimg::filters {
-lazy_filterchain filter::operator>>(filter&& f) {
-    return lazy_filterchain()
-        >> std::move(*this)
-        >> std::move(f);
-}
+//lazy_filterchain filter::operator>>(filter&& f) {
+//    return lazy_filterchain()
+//        >> std::move(*this)
+//        >> std::move(f);
+//}
 
-lazy_filterchain lazy_filterchain::operator>>(filter&& f) {
-  filters.push_back(f);
-  return *this;
-}
+//lazy_filterchain lazy_filterchain::operator>>(filter&& f) {
+//  filters.push_back(f);
+//  return *this;
+//}
 
-lazy_filterchain lazy_filterchain::operator()(const simg& input, simg& output) {
+lazy_filterchain& lazy_filterchain::evaluate(const simg& input, simg& output) {
   output = input;
 
   if(as_backwards)
     for(auto f = filters.rbegin(); f != filters.rend(); ++f)
-      (*f).get() >> output;
+      (*f)(output, output);
   else
     for(const auto& f: filters)
-      f.get() >> output;
+      f(output, output);
   
   return *this;
 }
 
-lazy_filterchain lazy_filterchain::operator>>(simg& image) {
-  (*this)(image, image);
+lazy_filterchain& lazy_filterchain::evaluate(simg& image) {
+  evaluate(image, image);
   return *this;
 }
 
-lazy_filterchain lazy_filterchain::operator--() {
+lazy_filterchain& lazy_filterchain::pop_end() {
   filters.pop_back();
   return *this;
 }
 
-lazy_filterchain lazy_filterchain::operator!() {
+lazy_filterchain& lazy_filterchain::reverse() {
   as_backwards = true;
   return *this;
 }
