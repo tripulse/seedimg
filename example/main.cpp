5a5b052c457059976c96f4e310da83f4db4bac0f
telugu-boy <h0l33fuk@gmail.com>
2020-08-31 01:04:59 -0600

    added tiff support

--- example/main.cpp
+++ example/main.cpp
@@ -43,7 +43,7 @@ int main() {
       // rotate_hue_i(a, 180);
       // v_mirror_i(a);
       // ocl::rotate_hue_i(a, -90);
-      seedimg::save("biol.jpg", a);
+      seedimg::save("biol.tiff", a);
       // bool b = seedimg::modules::jpeg::to("biol.jpg", a, 1);
 //      inversion_filter()         // [inverted]
 //          >> inversion_filter()  // [original]
