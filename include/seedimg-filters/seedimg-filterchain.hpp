8353eb16d4ae91fb93bbc44505846606db0f9e42
Santanu <bsantanu381@gmail.com>
2020-09-06 10:47:05 +0530

    Support both inplace and I/O based filters.
    This will store both kinds of filters in the internal queue (may change in future).
    
    reverse() method is not needed. Method names are shortened.

--- include/seedimg-filters/seedimg-filterchain.hpp
+++ include/seedimg-filters/seedimg-filterchain.hpp
@@ -1,4 +1,23 @@
-﻿#ifndef _SEEDIMG_FILTERCHAIN
+/**********************************************************************
+    seedimg - module based image manipulation library written in modern
+                C++ Copyright(C) 2020 tripulse
+
+    This program is free software : you can redistribute it and /
+    or modify it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation,
+    either version 3 of the License,
+    or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+************************************************************************/
+
+#ifndef _SEEDIMG_FILTERCHAIN
 #define _SEEDIMG_FILTERCHAIN
 
 #include <seedimg.hpp>
@@ -7,17 +26,13 @@
 
 
 namespace seedimg::filters {
-/** 
- * A linear chain of multiple filters which is lazily evaluated. For it being
- * lazily evaluated it can be re-used. The order of evaluating filters can be
- * customized by using boolean-negotiation operator.
+/**
+ * A linear chain of multiple filters which is lazily evaluated making it
+ * reusable multiple times. It contains both inplace and I/O-based filters.
  */
-class lazy_filterchain {
+class filterchain {
 protected:
-//  std::list<std::reference_wrapper<filter>> filters;
-
-  std::list<std::function<void(simg&, simg&)>> filters;
-  bool as_backwards;
+  std::list<std::function<void(simg&)>> filters;
 public:
   /**
    * @brief Push a function to end of the queue that follows
@@ -27,48 +42,49 @@ public:
    *
    * @param func a callable object.
    * @param args custom arguments to pass to callable.
+   *
+   * @typeparam inplace whether callback object does
+   * inplace image transformation. inplace functions have
+   * mostly "_i" as their suffix.
    */
+  template<bool inplace = false, class F, class... Args>
+  filterchain& add(F&& func, Args&&... args) {
+    filters.push_back([&](simg& input) -> void {
+      if constexpr(inplace)
+        func(input, args...);
+      else
+        func(input, input, args...);
+    });
 
-  // Why inlining?
-  // https://stackoverflow.com/q/10978642
-
-  // NOTE: excluding rvalue in 'args' causes the elements to be
-  // uninitialised for a very strange reason.
-  template<class F, class... Args>
-  lazy_filterchain& push_end(F&& func, Args&&... args) {
-    filters.push_back(
-          [&](simg& input, simg& output) -> void
-          { func(input, output, args...); });
     return *this;
   }
 
   /**
-   * @brief Evaluate the filters in the queue in a defined order,
-   * by default it does in forward order backwards can be set by
-   * .reverse() method. If the queue was empty the resulting image
-   * is the same as the given one.
+   * @brief Pop-off the most recently added filter in queue.
+   */
+  filterchain& pop() {
+    filters.pop_back();
+    return *this;
+  }
+
+  /**
+   * @brief Apply enqueued filters accumulatively from start to end.
    *
-   * @param input the initial image to apply the filters.
-   * @param output dump the resulting image.
+   * @param input image to apply filters on.
+   * @param output output of the accumulated result.
    */
-  lazy_filterchain& evaluate(const simg& input, simg& output);
+  filterchain& eval(const simg& in, simg& out) {
+    out = in;
+    for(const auto& f: filters)
+      f(out);
 
-  /**
-   * @brief Same as the other overload, except its done inplace.
-   */
-  lazy_filterchain& evaluate(simg& image);
+    return *this;
+  }
 
-  /**
-   * @brief Pop-off a filter from the very end of the queue.
-   */
-  lazy_filterchain& pop_end();
-
-  /**
-   * @brief Reverses the evaulation order, but doesn't reverse the
-   * actual ordering of filters in the internal queue. Sets a flag.
-   */
-  lazy_filterchain& reverse();
+  filterchain& eval(simg& img) {
+    eval(img, img);
+    return *this;
+  }
 };
 }
-
 #endif
